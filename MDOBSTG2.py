809e3288b0eb96b6a4e8c2391a5a34cd98b0053e
AlejoDuarte <alejandrod@mincka.com.au>
2021-04-05 20:02:07 -0500

    adding MH to BSTA and is rocking

--- MDOBSTG2.py
+++ MDOBSTG2.py
@@ -2,12 +2,11 @@ from scipy.optimize import fmin
 import BSDTA as BS
 import numpy as np
 import matplotlib.pyplot as plt
+import metropolis_hasting as MH
 
-plt.close('all')
-
+#------------------------------- 1. Theorerical Model ------------------------#
 
 def H(freq,f,z,S,Se,Nm,N):
-    
     H = np.zeros((Nm,Nm,N),dtype=np.complex_)
     for i in range(0,Nm):
         for j in range(0,Nm): 
@@ -18,32 +17,15 @@ def H(freq,f,z,S,Se,Nm,N):
                 ter2 = 1/((1-bkj)+2j*z[j]*bkj)
                 H[i,j,:] = (10.**S[i,j])*(ter1)*(ter2)+10.**Se
     return H
+#------------------------------- 2. Likelyhood ------------------------------#
 
-
-# def like(Y,freq_id,f,z,S,Se,phi,Nm,N,Nc): 
-#         Se = 10**Se
-#         S  = 10**S
-#         Li = 0
-#         SDKi =  np.trace(H(freq_id,f,z,S,Se,Nm,N))
-#         for i in range(N):
-            
-#             sk = Y[i,:].conj().reshape(Nc,1)*Y[i,:]
-#             print(sk)
-#             ti = 2*(SDKi[i]**2+2*Se*SDKi[i]+Se**2)
-#             L = np.log(np.pi*ti)+(np.trace(sk)-(SDKi[i]+Se))**2/ti
-#             Li = Li +L
-#             print(Li)
-#         return Li
-
-def new_lik(Y,freq_id,x,Nm,N,Nc):
-    # breakpoint()
+def MDOF_lkhood(Y,freq_id,x,Nm,N,Nc):
     f = x[:Nm]
     z = x[ Nm:2*Nm]
     S= np.diag(x[ 2*Nm:3*Nm])
     Se=x[-1]    
     Li = 0
     SDKi =  np.abs(np.trace(H(freq_id,f,z,S,Se,Nm,N)))
-    # Ho =  H(freq_id,f,z,S,Se,Nm,N)
     for i in range(N):
         sk = Y[i,:].conj().reshape(Nc,1)*Y[i,:]
         ti = 2*(SDKi[i]**2+2*(10.**Se)*SDKi[i]+(10.**Se)**2)
@@ -51,8 +33,8 @@ def new_lik(Y,freq_id,x,Nm,N,Nc):
         Li = Li +L
     return Li
 
-def Modes(freq_id,x,Nm,N,Nc):
-    # breakpoint()
+#------------------------------- 3. Model ------------------------------------#
+def Model(freq_id,x,Nm,N,Nc):
     f = x[:Nm]
     z = x[ Nm:2*Nm]
     S= np.diag(x[ 2*Nm:3*Nm])
@@ -60,6 +42,21 @@ def Modes(freq_id,x,Nm,N,Nc):
     Li = 0
     SDKi=  np.abs(np.trace(H(freq_id,f,z,S,Se,Nm,N)))
     return SDKi
+#------------------------------- 4. Map  -------------------------------------#
+def ID_BSTA_MDOF(Yxx,freq_id,x,Nm,N,Nc,PL=True):
+    likelyhood = lambda x:MDOF_lkhood(Yxx,freq_id,x,Nm,N,Nc)
+    opt = fmin(func=likelyhood ,x0=xo,maxiter= 5000,xtol=1e-12, ftol=1e-12)
+    if PL == True:
+        modelo  = []
+        for i in range(N):
+            sk = Yxx[i,:].conj().reshape(Nc,1)*Yxx[i,:]
+            modelo = np.abs(np.append(modelo,np.trace(sk)/Nc))
+        SDKi = Model(freq_id,opt,Nm,N,Nc)
+        plt.figure()
+        plt.plot(freq_id,10*np.log10(SDKi))
+        plt.plot(freq_id,10*np.log10(modelo))
+    return opt
+
 
 #------------------------------- Data ----------------------------------------#
 fs = 95
@@ -68,35 +65,32 @@ Acc = Acco[:,:3]
 Nd,Nc = Acc.shape
 fo = 3
 fi = 4.2
+#------------------------------- t2frequencydomain----------------------------#
 Yxx,freq_id,N = BS.PSD_FORMAT(Acc,fs,fo,fi)
+# Target Frequencies
 f=[3.72,3.89]
 Nm = 2
 z = [ 5.0e-03, 5.0e-03]
 S = [-6,-6]
 Se = [-5]
 xo = [*f,*z,*S,*Se]
+#------------------------------- Engine --------------------------------------#
+ID_BSTA_MDOF(Yxx,freq_id,xo,Nm,N,Nc,PL=True)
 
-SDKi = Modes(freq_id,xo,Nm,N,Nc)
-plt.figure()
+
+
+std_tr = [0.001,0.001,0.00005,0.00005,0.1,0.1,0.1]
+tr_model = lambda x: MH.transition_model(x,std_tr)
+
+pri_lim = [[3.6,3.8],[3.7,4],[0,0.05],[0,0.05],[-10,0],[-10,0],[-20,-1]]
+pri_model = lambda x: MH.prior(x,pri_lim)
+
+lik= lambda x,y: -MDOF_lkhood(y,freq_id,x,Nm,N,Nc)
+accepted,rejected,prob = MH.metropolis_hastings(lik,pri_model, tr_model,xo,3000,Yxx)
+
+
+# plt.plot(accepted)
+opt2 = np.mean(accepted,axis =0)
+SDKi = Model(freq_id,opt2,Nm,N,Nc)
 plt.plot(freq_id,10*np.log10(SDKi))
-plt.plot(freq_id,10*np.log10(Yxx))
-likelyhood = lambda x:new_lik(Yxx,freq_id,x,Nm,N,Nc)
-#---------------- Optimize likelyhood ----------------#
-
-# opt = fmin(func=likelyhood ,x0=xo)
-opt = fmin(func=likelyhood ,x0=xo,maxiter= 5000,xtol=1e-12, ftol=1e-12)
-SDKi = Modes(freq_id,opt,Nm,N,Nc)
-
-modelo = []
-for i in range(N):
-    sk = Yxx[i,:].conj().reshape(Nc,1)*Yxx[i,:]
-    modelo = np.abs(np.append(modelo,np.trace(sk)/Nc))
-        
-        
-plt.figure()
-plt.plot(freq_id,10*np.log10(SDKi),color = 'red')
-plt.plot(freq_id,10*np.log10(modelo),color= 'k')
-
-
-
-
+plt.plot(freq_id,10*np.log10(Yxx**2))
--- metropolis_hasting.py
+++ metropolis_hasting.py
@@ -1,14 +1,40 @@
 import numpy as np
 from scipy import stats
 from tqdm import tqdm
-#import warnings
-#import time    
+
+
+
 
 def transition_model(x,std):
+    """
+    Parameters
+    ----------
+    x : list
+        Mean values for proposed distribution.
+    std : list
+        Standard deviation for proposed distribution.
+    Returns
+    -------
+    Proposal PDF
+    TYPE
+        Numpy array.
+    """
     return np.random.normal(x,std,(len(x),))
 
-
 def prior(x,lim_pri):
+    """
+    Parameters
+    ----------
+    x : list
+        current values markov chain.
+    lim_pri : list
+        boundaries for uniform prior distribution.
+
+    Returns
+    -------
+    int
+        DESCRIPTION.
+    """
     sw =0
     i =0
     while sw == 0:
@@ -22,26 +48,55 @@ def prior(x,lim_pri):
             sw = 1
     return 0
 
-def acceptance(x, x_new):
+def acceptance_rule(x, x_new):
+    """
+    
+
+    Parameters
+    ----------
+    x : list
+        current values markov chain.
+    x_new : list
+        next values markov chain.
+
+    Returns
+    -------
+    TYPE
+        bolean 
+
+    """
     if x_new>x:      
         return True
     else:
-        accept=accept= 0.001 #np.random.uniform(0,1)
+        accept= np.random.uniform(0,1)
     return (accept < (np.exp(x_new-x)))
 
-# def model(x,z):
-#     y = x[0]*z**2+x[1]
-#     return y
  
 def likelyhood(x,y,model):
+    """
+    
 
+    Parameters
+    ----------
+    x : list
+        current value of MCMC.
+    y : Array
+        FFT values.
+    model : Function
+       Theoretical PSD.
+    Returns
+    -------
+    lik : float
+        likelyhood value
+
+    """
     yhat = model(x[:-1])
     err = np.linalg.norm(yhat-y)
     print(err)
     lik = -err/((10**-12))
     return lik
     
-def metropolis_hastings(likelihood_computer,prior, transition_model, param_init,iterations,data,acceptance_rule):
+def metropolis_hastings(likelihood_computer,prior, transition_model, param_init,iterations,data):
     x = param_init
     accepted = []
     rejected = []  
@@ -60,50 +115,49 @@ def metropolis_hastings(likelihood_computer,prior, transition_model, param_init,
             
     return np.array(accepted), np.array(rejected),prob
 
-from MDOF_LSQ import CPSD,Model
-import matplotlib.pyplot as plt
+# from MDOF_LSQ import CPSD,Model
+# import matplotlib.pyplot as plt
 
-Nm = 3
-fs = 95
-Acco =  np.loadtxt('Data/Sp_Acc_11_02_12_7_18.txt',delimiter = ',')
-Acc = Acco[:,:3]
-Nc = Acc.shape[1]
-Nm = 2
+# Nm = 3
+# fs = 95
+# Acco =  np.loadtxt('Data/Sp_Acc_11_02_12_7_18.txt',delimiter = ',')
+# Acc = Acco[:,:3]
+# Nc = Acc.shape[1]
+# Nm = 2
 
-ff,cpsd,N,Fc = CPSD(Acc,fs,Nc,3.4,4)
+# ff,cpsd,N,Fc = CPSD(Acc,fs,Nc,3.4,4)
 
-f=[3.719,3.8870]
+# f=[3.719,3.8870]
 
-z = [0.005,0.0053]
-S = [-8,-8]
-Se = [-6]
-std = [-5]
-xo = [*f,*z,*S,*Se,*std]
-m_model = lambda x: Model(x,ff,Nm,N,Fc)
+# z = [0.005,0.0053]
+# S = [-8,-8]
+# Se = [-6]
+# std = [-5]
+# xo = [*f,*z,*S,*Se,*std]
+# m_model = lambda x: Model(x,ff,Nm,N,Fc)
 
-std_tr = [0.001,0.001,0.001,0.001,0.5,0.5,0.5,1]
-tr_model = lambda x: transition_model(x,std_tr)
+# std_tr = [0.001,0.001,0.001,0.001,0.5,0.5,0.5,1]
+# tr_model = lambda x: transition_model(x,std_tr)
 
-pri_lim = [[3.6,3.8],[3.7,4],[0,0.05],[0,0.05],[-20,0],[-20,0],[-20,-1],[-30,-1]]
-pri_model = lambda x: prior(x,pri_lim)
+# pri_lim = [[3.6,3.8],[3.7,4],[0,0.05],[0,0.05],[-20,0],[-20,0],[-20,-1],[-30,-1]]
+# pri_model = lambda x: prior(x,pri_lim)
 
-lik= lambda x,y: likelyhood(x,y,m_model)
-
-
-
-accepted,rejected,prob = metropolis_hastings(lik,pri_model, tr_model,xo,1000,cpsd,acceptance)
+# lik= lambda x,y: likelyhood(x,y,m_model)
 
 
-    
+
+# accepted,rejected,prob = metropolis_hastings(lik,pri_model, tr_model,xo,1000,cpsd)
 
 
-plt.close('all')
-plt.figure()
-plt.plot(accepted)
-plt.figure()
-xopt = np.mean(accepted,axis = 0)[:-1]
-plt.plot(ff,10*np.log10(m_model(xopt)))
-plt.plot(ff,10*np.log10(cpsd))
+
+
+# plt.close('all')
+# plt.figure()
+# plt.plot(accepted)
+# plt.figure()
+# xopt = np.mean(accepted,axis = 0)[:-1]
+# plt.plot(ff,10*np.log10(m_model(xopt)))
+# plt.plot(ff,10*np.log10(cpsd))
 # plt.plot(ff,m_model(xo))
 
 # zo = np.linspace(0,10,100)
